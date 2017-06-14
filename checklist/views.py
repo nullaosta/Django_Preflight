7758b707a518ee2d7136fa6c492cdda008597f4a
michaelvincerra <michael.vincerra@gmail.com>
2017-06-14 13:55:51 -0700

    Adds function 'checked'for future work

--- checklist/views.py
+++ checklist/views.py
@@ -12,3 +12,18 @@ def itinerary(request):
     context = {'items': items}
 
     return render(request, 'django_preflight.html', context)
+
+
+# def checked(request, pk=True):
+#     """
+#     Creates a new Ice Cream record in the database
+#     from the url.
+#
+#     A view is a function that takes an HTTP request
+#     and returns an HTTP response.
+#
+#     """
+#     item_checked = ListItem.objects.get(pk=pk)
+#     context = {'item_checked': item_checked}
+#
+#     return render(request, 'django_preflight.html', context)
