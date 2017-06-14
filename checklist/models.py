440455f4725d49621904ebd327749e7e8e030f66
Rebecca Hanlon <ryhanlon@gmail.com>
2017-06-14 11:51:00 -0700

    Made model for the check list (title, name, date, priority.

--- checklist/models.py
+++ checklist/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
+import datetime
+
 
 class ListItem(models.Model):
     """
     Items to do, tasks to complete; A single item in a To Do list.
+    Used code from a tutorial by James Bennett at sitepoint.com.
 
     """
+
+    PRIORITY_CHOICES = (
+        (1, 'Low'),
+        (2, 'Normal'),
+        (3, 'High'),
+    )
+
+    title = models.CharField(maxlength=250)
+    created_date = models.DateTimeField(default=datetime.datetime.now)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-priority', 'title']
