5014fba0dcad6a98fbd66c3566421d9e17cf1ed2
michaelvincerra <michael.vincerra@gmail.com>
2017-06-14 11:41:22 -0700

    Creates itinerary function for ListItem class, for which there shall be no substitute.

--- checklist/views.py
+++ checklist/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import ListItem
 
-# Create your views here.
+
+
+def itinerary(request):
+    """
+    Create an introductory page for Django preflight planner. 
+    
+    """
+    items = ListItem.objects.all()
+    context = {'items': items}
+
+    return render(request, 'django_preflight.html', context)
